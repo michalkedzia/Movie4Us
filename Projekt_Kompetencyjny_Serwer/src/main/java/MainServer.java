77bd86171f194fbf661f8881fa2627c3b41e5592
michalkedzia <222463@edu.p.lodz.pl>
2020-11-11 19:20:15 +0100

    init

---
+++ Projekt_Kompetencyjny_Serwer/src/main/java/MainServer.java
@@ -0,0 +1,63 @@
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+import java.util.concurrent.*;
+
+import log.MyLOG;
+import model.*;
+import handlers.*;
+
+public class MainServer {
+    public static void main(String[] args) throws IOException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        ServerSocket serverSocket = new ServerSocket(5000);
+        Gson gson = new Gson();
+        Message message;
+
+
+        ConcurrentMap<String,ClientHandler> clientsMap = new ConcurrentHashMap<>();
+
+//        List<ClientHandler> clientHandlerList = new CopyOnWriteArrayList<>();
+        BlockingQueue<Message> messageQueue = new LinkedBlockingDeque<>();
+        executorService.execute(new MessageQueueHandler(clientsMap,messageQueue));
+
+
+        while (true){
+            System.out.println("server start");
+            Socket socket = serverSocket.accept();
+            System.out.println(socket.toString());
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            ClientHandler clientHandler;
+            String json = bufferedReader.readLine();
+            message = gson.fromJson(json,Message.class);
+            if(message.getAction().equals("login")){
+                clientHandler = new ClientHandler(socket,printWriter,bufferedReader,messageQueue);
+                clientHandler.setUsername(message.getUsername());
+                clientsMap.put(message.getUsername(),clientHandler);
+                executorService.execute(clientHandler);
+            }else {
+                MyLOG.myLOG("login error " + socket);
+            }
+
+
+//            ClientHandler clientHandler = new ClientHandler(socket, printWriter, bufferedReader,messageQueue);
+//            clientHandlerList.add(clientHandler);
+//            executorService.execute(clientHandler);
+        }
+    }
+
+
+
+
+
+
+}
---
+++ Projekt_Kompetencyjny_Serwer/src/main/java/model/Message.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Message {
+    private String username;
+    private String connectedUser;
+    private String action;
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "username='" + username + '\'' +
+                ", connectedUser='" + connectedUser + '\'' +
+                ", action='" + action + '\'' +
+                '}';
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getConnectedUser() {
+        return connectedUser;
+    }
+
+    public void setConnectedUser(String connectedUser) {
+        this.connectedUser = connectedUser;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+}
