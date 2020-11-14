481c8da28ad04159fba6217813a86087bf0affa9
Bartosz Kaczor <222453@edu.p.lodz.pl>
2020-11-14 05:13:13 +0100

    Add API request class, JSON object to data class formatter and data classes.
    Add API request class to simplify sending and receving requests from TMDB database. Add data classes and JSON object formatter to simplify parsing and storing JSON data.
    
    1. Add APICaller
    2. Add APIjsonFormatter - JSON object to data class formatter
    3. Add MovieData and PageMovieData - data classes for storing info about movies

---
+++ Projekt_Kompetencyjny_Serwer/src/main/java/api/APIjsonFormatter.java
@@ -0,0 +1,33 @@
+package api;
+
+import api.data.MovieData;
+import api.data.PageMovieData;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import exceptions.APIException;
+
+import java.util.ArrayList;
+
+public class APIjsonFormatter {
+  PageMovieData pageMovieData;
+
+  PageMovieData parseToObject(JsonObject rootObj) throws APIException.WrongJsonObjectException {
+    if (!rootObj.has("results")) {
+      throw new APIException.WrongJsonObjectException("Wrong json object passed");
+    }
+
+    ArrayList<MovieData> movieDataArray = new ArrayList<>();
+
+    Gson gson = new Gson();
+    pageMovieData = gson.fromJson(rootObj.toString(), PageMovieData.class);
+
+    for (int i = 0; i < rootObj.getAsJsonArray("results").size(); i++) {
+      movieDataArray.add(
+          gson.fromJson(rootObj.getAsJsonArray("results").get(i).toString(), MovieData.class));
+    }
+    pageMovieData.movieDataArray = movieDataArray;
+    pageMovieData.results_on_page = movieDataArray.size();
+
+    return pageMovieData;
+  }
+}
---
+++ Projekt_Kompetencyjny_Serwer/src/main/java/api/data/PageMovieData.java
@@ -0,0 +1,25 @@
+package api.data;
+
+import java.util.ArrayList;
+
+public class PageMovieData {
+  public int page;
+  public int total_results;
+  public int total_pages;
+  public ArrayList<MovieData> movieDataArray;
+  public int results_on_page;
+
+  @Override
+  public String toString() {
+    return "PageMovieData{"
+        + "\npage="
+        + page
+        + ", \ntotal_results="
+        + total_results
+        + ", \ntotal_pages="
+        + total_pages
+        + ", \nresults_on_page="
+        + results_on_page
+        + '}';
+  }
+}
