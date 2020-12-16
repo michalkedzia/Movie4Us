f8084fd77ad80012c92fc4cfed2c403b8474e0e9
michalkedzia <222463@edu.p.lodz.pl>
2020-12-17 00:14:20 +0100

    Merge

--- Projekt_Kompetencyjny/app/src/main/java/com/movie4us/SignUp.java
+++ Projekt_Kompetencyjny/app/src/main/java/com/movie4us/SignUp.java
@@ -76,7 +76,7 @@ public class SignUp extends AppCompatActivity {
 
                                         PutData putData =
                                                 new PutData(
-                                                        "http://192.168.1.101/LoginRegister/signup.php", "POST", field, data);
+                                                        "http://169.254.250.43/LoginRegister/signup.php", "POST", field, data);
                                         if (putData.startPut()) {
                                             if (putData.onComplete()) {
                                                 progressBar.setVisibility(View.GONE);
