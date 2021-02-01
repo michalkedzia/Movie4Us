e211079f162f260ee1380fed98f57282a2ce08a1
matim1324 <matim1324@gmail.com>
2021-02-01 14:50:46 +0100

    Javadoc.

--- Projekt_Kompetencyjny/app/src/main/java/com/movie4us/Match.java
+++ Projekt_Kompetencyjny/app/src/main/java/com/movie4us/Match.java
@@ -2,22 +2,39 @@ package com.movie4us;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 import data.MovieData;
 
+/**
+ * Klasa Match obsługuje nowe okno aplikacji. Wywoływana z CardSwipeActivity (skąd przekazywane są
+ * odpowiednie dane, tj. informacje o wybranym filmie) tworzy okno/widok aplikacji wybranych filmów
+ * i uruchamia je.
+ */
 public class Match extends AppCompatActivity {
+  /** Obiekt przechowywujacy informacje o obrazie wyświetlanym w widoku XML'a aplikacji. */
+  private ImageView imageView;
 
-  private ImageView imageView, imageProvider;
-  private TextView title, linkStatus, links;
+  /** Pola tekstowe ustawiane w widoku XML'a aplikacji. */
+  private TextView title, linkStatus;
 
+  /**
+   * Metoda ustawia nową scenę aplikacji pokazującą informację o wybranym filmie. Zostaje ustawiony
+   * nowy widok, wyświetlający wybrany film oraz dodatkowe informacje o dostępności filmu (w
+   * zależności od ilości providerów) z klasy MovideData przekazywanej z odpowiednimi parametrami z
+   * poprzedniego widoku aplikacji.
+   *
+   * @param savedInstanceState Służy do przekazania informacji o stanie aktywności aplikacji
+   *     Android'a. W przypadku zmiany orientacji, zamknięcia aplikacji itp., które prowadzą do
+   *     ponownego wywołania metody onCreate, pakiet ten może zostać użyty do załadowania informacji
+   *     o poprzednim stanie aplikacji.
+   */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -26,14 +43,20 @@ public class Match extends AppCompatActivity {
     Gson gson = new Gson();
 
     MovieData movieData = gson.fromJson(intent.getStringExtra("movie"), MovieData.class);
-    System.out.println("id: " + movieData.id + ", tytul: " +  movieData.title + ", " + movieData.watchProviderData.getPLProvider());
-
+    System.out.println(
+        "id: "
+            + movieData.id
+            + ", tytul: "
+            + movieData.title
+            + ", "
+            + movieData.watchProviderData.getPLProvider());
 
     imageView = findViewById(R.id.filmImage);
     title = findViewById(R.id.Title);
     linkStatus = findViewById(R.id.LinkStatus);
+
     LinearLayout layout = findViewById(R.id.imageContainer);
-    LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+    LayoutInflater inflater = LayoutInflater.from(this);
 
     loadImageFromUrl(movieData.getPoster_path(), imageView);
     title.setText("'" + movieData.title + "'");
@@ -41,27 +64,32 @@ public class Match extends AppCompatActivity {
     int numberOfProviders = movieData.watchProviderData.getPLProvider().size();
     System.out.println("numbers of providers: " + numberOfProviders);
 
-    if(numberOfProviders == 0) {
+    if (numberOfProviders == 0) {
       linkStatus.setText("Brak w PL");
     } else {
-      for(int i=0; i<numberOfProviders; i++){
-        layoutParams.setMargins(20, 20, 20, 20);
-        layoutParams.height = 150;
-        layoutParams.width = 150;
-        layoutParams.gravity = Gravity.CENTER;
-        ImageView providerImage = new ImageView(this);
+      for (int i = 0; i < numberOfProviders; i++) {
+        View view = inflater.inflate(R.layout.provider_item, layout, false);
 
-        loadImageFromUrl(movieData.watchProviderData.getPLProvider().get(i), providerImage);
-        providerImage.setLayoutParams(layoutParams);
+        TextView text = view.findViewById(R.id.providerName);
+        text.setText(movieData.watchProviderData.getPLProvider().get(i));
+        ImageView image = view.findViewById(R.id.providerImage);
+        loadImageFromUrl(movieData.watchProviderData.getPLProviderLogo().get(i), image);
 
-        layout.addView(providerImage);
+        layout.addView(view);
       }
     }
   }
 
+  /**
+   * Metoda ładuje z podanej ścieżki url (wyciąganej z bazy filmów) obraz przedstawiający dany film,
+   * bądź logo providera do obiektu ImageView za pomocą biblioteki do pobierania i buforowania
+   * obrazów dla Android'a 'Picasso'.
+   *
+   * @param url Sciezka url skąd pobierany jest obraz.
+   * @param image Widok do którego ma być załadowany obraz.
+   * @see Picasso
+   */
   private void loadImageFromUrl(String url, ImageView image) {
     Picasso.get().load(url).into(image);
   }
-
-
 }
--- Projekt_Kompetencyjny/app/src/main/java/dialog/InfoDialog.java
+++ Projekt_Kompetencyjny/app/src/main/java/dialog/InfoDialog.java
@@ -6,10 +6,18 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatDialogFragment;
 import com.movie4us.GenreSelectionActivity;
 
+/**
+ * Klasa InfoDialog tworzy Dialog wyświetlający ustawione informacje. W zależności od ustawienia
+ * parametrów, możemy wyświetlić tytuł dialogu, informację jaką dany dialog ma zawierać, oraz polę
+ * klasy GenreSelectionActivity, w której jest ten dialog tworzony.
+ */
 public class InfoDialog extends AppCompatDialogFragment {
 
+  /** Tytuł wyświetlanego dialogu */
   private String title;
+  /** Tekst wyświetlanej wiadomości */
   private String message;
+  /** Obiekt klasy GenreSelectionActivity w której jest wyświetlany dialog */
   private GenreSelectionActivity genreSelectionActivity;
 
   public InfoDialog(String title, String message, GenreSelectionActivity genreSelectionActivity) {
@@ -18,6 +26,19 @@ public class InfoDialog extends AppCompatDialogFragment {
     this.genreSelectionActivity = genreSelectionActivity;
   }
 
+  /**
+   * Metoda tworząca za pomocą bilbitoeki AlertDialog, dialog z ustawioną informacją dla
+   * użytkownika. W przypadku klasy GenreSelectionActivity, gdzie dialog jest wyświetlany jest to
+   * informacja z wyjściem drugiego użytkownika z aktualnego okna.
+   *
+   * <p>Po potwierdzeniu wyświetlanego dialogu z informacją o odrzuceniu przez jej zatwierdzenie,
+   * następuje wyjście z activity w którym ten dialog został uruchomiony. W tym przypadku jest to
+   * GenreSelectionActivity.
+   *
+   * @param savedInstanceState Służy do przekazania informacji o stanie aktywności aplikacji
+   *     Android'a.
+   * @return builder.create() zwraca wywołanie dialogu
+   */
   @Override
   public Dialog onCreateDialog(Bundle savedInstanceState) {
 
